2c8c08a8a20df7ff3845f96e1d2dbd11e593bbb1
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2021-06-05 16:32:00 +0200

    Update actix hello-world example as baseline

--- uppercut-mio-server/baseline-actix/src/main.rs
+++ uppercut-mio-server/baseline-actix/src/main.rs
@@ -1,13 +1,15 @@
-// https://github.com/actix/examples/blob/master/hello-world/src/main.rs
-
 use actix_web::{web, App, HttpServer};
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
+    // std::env::set_var("RUST_LOG", "actix_web=info");
+    // env_logger::init();
 
-    HttpServer::new(|| App::new().service(web::resource("/").to(|| async { "hello\n" })))
+    HttpServer::new(|| {
+        App::new()
+            // .wrap(middleware::Logger::default())
+            .service(web::resource("/").to(|| async { "hello\n" }))
+    })
         .bind("0.0.0.0:9000")?
         .run()
         .await
