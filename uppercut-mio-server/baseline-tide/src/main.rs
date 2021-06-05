f410dab4bf5987ac5ee4b68d800cd0d6ae853716
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2021-06-05 16:32:16 +0200

    Update tide hello-world example as baseline

--- uppercut-mio-server/baseline-tide/src/main.rs
+++ uppercut-mio-server/baseline-tide/src/main.rs
@@ -1,10 +1,8 @@
-
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
   //tide::log::start();
   let mut app = tide::new();
   app.at("/").get(|_| async { Ok("hello\n") });
-  app.listen("127.0.0.1:9000").await?;
+  app.listen("0.0.0.0:9000").await?;
   Ok(())
 }
-
