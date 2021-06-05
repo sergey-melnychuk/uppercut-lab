99c192f4c9a88bf3cd9f161f58e21fcc1f0445d3
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2021-06-05 16:32:32 +0200

    Add proper Date field for HTTP response

--- uppercut-mio-server/src/protocol.rs
+++ uppercut-mio-server/src/protocol.rs
@@ -1,11 +1,14 @@
 extern crate log;
 extern crate env_logger;
 use log::debug;
+use chrono::{DateTime, Utc};
 
 use parsed::stream::ByteStream;
 use parsed::http::{parse_http_request, Request, Header, Response};
 
 fn handle(_: Request) -> Response {
+    let now: DateTime<Utc> = Utc::now();
+
     Response {
         protocol: "HTTP/1.1".to_string(),
         code: 200,
@@ -13,8 +16,7 @@ fn handle(_: Request) -> Response {
         headers: vec![
             Header { name: "Content-Length".to_string(), value: "6".to_string(), },
             Header { name: "Content-Type".to_string(), value: "text/plain; charset=utf-8".to_string(), },
-            // TODO FIXME get rid of hard-coded date!
-            Header { name: "Date".to_string(), value: "Thu, 04 Mar 2021 21:54:41 GMT".to_string(), },
+            Header { name: "Date".to_string(), value: now.to_rfc2822(), },
         ],
         content: "hello\n".as_bytes().to_vec(),
     }
